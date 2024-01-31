f44b9e02e2e88ed16c5c6de7f09fd2810a57eb0b
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-31 22:27:02 +0530

    Display map and marker on all cities

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,21 +1,45 @@
+import { useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { useCity } from '../contexts/CityContext';
 import styles from './css/Map.module.css';
 
 function Map() {
-	// Programmatic navigation using useNavigate
-	// generally used for navigation on submitting <form>s
 	const navigate = useNavigate();
+	const { cities } = useCity();
 
 	const [searchParams, setSearchParams] = useSearchParams();
 	const lat = searchParams.get('lat');
 	const lng = searchParams.get('lng');
 
+	const [mapPosition, setMapPosition] = useState([39, -9]);
+
 	return (
-		<div className={styles.mapContainer} onClick={() => navigate('form')}>
-			<h1>Map</h1>
-			<h2>
-				{lat} {lng}
-			</h2>
+		<div className={styles.mapContainer}>
+			<MapContainer
+				center={mapPosition}
+				zoom={13}
+				scrollWheelZoom={true}
+				// set the styles.map property to make height: 100%
+				className={styles.map}
+			>
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				/>
+
+				{/* Render a marker and popup on map for all cities */}
+				{cities.map(city => (
+					<Marker
+						position={[city.position.lat, city.position.lng]}
+						key={city.id}
+					>
+						<Popup>
+							{city.emoji} {city.cityName}
+						</Popup>
+					</Marker>
+				))}
+			</MapContainer>
 		</div>
 	);
 }
