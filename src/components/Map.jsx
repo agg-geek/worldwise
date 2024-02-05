b4076e26e677c6aab8b4efe4145d5236e92d2344
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-02-05 19:55:31 +0530

    Move map to city location on choosing city

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,26 +1,37 @@
 import { useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import { useCity } from '../contexts/CityContext';
 import styles from './css/Map.module.css';
+
+// see explanation above searchParams first
+// every change in react leaflet occurs via components
+function CenterMap({ position }) {
+	const map = useMap();
+	map.setView(position);
+	return null; // you need to return JSX, so just return null here
+}
 
 function Map() {
 	const navigate = useNavigate();
 	const { cities } = useCity();
 
+	// you use the lat and lng from the URL query to go to a particular position
+	// however, when you are browsing a city and you change it,
+	// then the map doesn't change its position, so you need to make leaflet change it
 	const [searchParams, setSearchParams] = useSearchParams();
-	const lat = searchParams.get('lat');
-	const lng = searchParams.get('lng');
+	const lat = searchParams.get('lat') ?? 39;
+	const lng = searchParams.get('lng') ?? -9;
 
-	const [mapPosition, setMapPosition] = useState([39, -9]);
+	// const [mapPosition, setMapPosition] = useState([39, -9]);
 
 	return (
 		<div className={styles.mapContainer}>
 			<MapContainer
-				center={mapPosition}
-				zoom={13}
+				// center={mapPosition}
+				center={[lat, lng]}
+				zoom={8}
 				scrollWheelZoom={true}
-				// set the styles.map property to make height: 100%
 				className={styles.map}
 			>
 				<TileLayer
@@ -28,7 +39,6 @@ function Map() {
 					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 				/>
 
-				{/* Render a marker and popup on map for all cities */}
 				{cities.map(city => (
 					<Marker
 						position={[city.position.lat, city.position.lng]}
@@ -39,6 +49,8 @@ function Map() {
 						</Popup>
 					</Marker>
 				))}
+
+				<CenterMap position={[lat, lng]} />
 			</MapContainer>
 		</div>
 	);
